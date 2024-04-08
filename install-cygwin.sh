9a9682db3bd0ae65674196735c69bbca70191c40
Michael Cotterell <mepcotterell@gmail.com>
2024-04-08 17:01:57 -0400

    updated test installers

--- install-cygwin.sh
+++ install-cygwin.sh
@@ -33,5 +33,7 @@ if type -P MobApt >/dev/null 2>&1; then
     declare -r APT="$(type -P MobApt)"
 fi
 
+tput cols
+
 2>&1 "${APT}" install -y coreutils curl cygutils git ncurses unzip wget \
     | sed 's|^|[apt-get] |g'
